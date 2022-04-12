da068b1ecc29e579df253e9072b76ac0b64bcb37
sylla ibrhim <s67986601@gmail.com>
2022-04-12 16:28:02 +0100

    build backend api

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -2,9 +2,37 @@ const PostModel = require("../models/post.models.js");
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/post.controller.js");
+const multer = require("multer");
 
-router.route("/").get(postController.readPost).post(postController.createPost);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __dirname + "../../client/public/uploads/posts");
+  },
+  filename: (req, file, cb) => {
+    if (file !== null) {
+      try {
+        if (
+          file.mimetype == "image/jpg" ||
+          file.mimetype == "image/png" ||
+          file.mimetype == "image/jpeg" &&
+          (file.size > 500000) 
+        ){
+          cb(null, Date.now() + file.originalname);
+        }else {
+          cb(null,null)
+        }
+          
 
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
+});
+const postStorage = multer({ storage: storage });
+
+router.route("/").get(postController.readPost);
+router.post("/", postStorage.single("picture"), postController.createPost);
 router
   .route("/:id")
   .put(postController.updatePost)
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,47 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.Controller.js");
 const userController = require("../controllers/user.controller.js");
+const uploadController = require('../controllers/upload.controller.js');
+
+const multer = require("multer");
+const { exists } = require("../models/post.models.js");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __dirname + "../../client/public/uploads/profil");
+  },
+  filename: (req, file, cb) => {
+    if (file !== null) {
+      try {
+        if (
+          file.mimetype == "image/jpg" ||
+          file.mimetype == "image/png" ||
+          file.mimetype == "image/jpeg" 
+        ){
+          cb(null, Date.now() + file.originalname);
+        }else {
+          throw Error('votre fichier nest pas trop')
+        }
+          
+
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
+  onError : function(err, next) {
+    console.log('error', err);
+    next(err);
+  }
+});
+const userStorage = multer({ storage: storage });
 
 router
   .route("/register")
   .get(userController.getAllUsers)
   .post(authController.signUp);
+
+router.get('/', userController.getAllUsers);
+
 
 router.route("/login/").post(authController.signIn);
 
@@ -21,5 +57,9 @@ router
 router.route("/follow/:id").patch(userController.follow);
 
 router.route("/unfollow/:id").patch(userController.unfollow);
+
+
+// upload
+router.post("/upload", userStorage.single("picture"), uploadController.uploadProfil);
 
 module.exports = router;
